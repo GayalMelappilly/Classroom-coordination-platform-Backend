3c94f044db0baa67cce69e3e0637d7089a9c0b3b
Gayal Melappilly <gayalsunil@gmail.com>
2024-06-12 01:24:03 +0530

    Off to merge

--- middlewares/whiteListIp.ts
+++ middlewares/whiteListIp.ts
@@ -6,40 +6,40 @@ import { Buffer } from 'buffer';
 dotenv.config();
 
 async function addCurrentIPToWhitelist() {
-    try {
-        const ip = await publicIpv4();
-        const publicKey = process.env.MONGODB_PUBLIC_KEY;
-        const privateKey = process.env.MONGODB_PRIVATE_KEY;
-        const projectId = process.env.MONGODB_PROJECT_ID;
+    // try {
+    //     const ip = await publicIpv4();
+    //     const publicKey = process.env.MONGODB_PUBLIC_KEY;
+    //     const privateKey = process.env.MONGODB_PRIVATE_KEY;
+    //     const projectId = process.env.MONGODB_PROJECT_ID;
 
-        if (!ip || !publicKey || !privateKey || !projectId) {
-            console.error("Missing environment variables.");
-            return;
-        }
+    //     if (!ip || !publicKey || !privateKey || !projectId) {
+    //         console.error("Missing environment variables.");
+    //         return;
+    //     }
 
-        const whitelistUrl = `https://cloud.mongodb.com/api/atlas/v1.0/groups/${projectId}/accessList`;
+    //     const whitelistUrl = `https://cloud.mongodb.com/api/atlas/v1.0/groups/${projectId}/accessList`;
 
-        // Create basic auth header
-        const auth = Buffer.from(`${publicKey}:${privateKey}`).toString('base64');
+    //     // Create basic auth header
+    //     const auth = Buffer.from(`${publicKey}:${privateKey}`).toString('base64');
 
-        const response = await axios.post(
-            whitelistUrl,
-            {
-                ipAddress: ip,
-                comment: 'Added by script'
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Basic ${auth}`
-                }
-            }
-        );
+    //     const response = await axios.post(
+    //         whitelistUrl,
+    //         {
+    //             ipAddress: ip,
+    //             comment: 'Added by script'
+    //         },
+    //         {
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //                 'Authorization': `Basic ${auth}`
+    //             }
+    //         }
+    //     );
 
-        console.log('IP added to whitelist:', response.data);
-    } catch (error:any) {
-        console.error('Error adding IP to whitelist:', error.response ? error.response.data : error.message);
-    }
+    //     console.log('IP added to whitelist:', response.data);
+    // } catch (error:any) {
+    //     console.error('Error adding IP to whitelist:', error.response ? error.response.data : error.message);
+    // }
 }
 
 export default addCurrentIPToWhitelist;
